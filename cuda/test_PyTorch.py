d50991f3af257b3e94b0830c8c23528903ff6f7b
HyTruongSon <sonpascal93@gmail.com>
2020-10-02 01:26:20 -0500

    Time measuring

--- cuda/test_PyTorch.py
+++ cuda/test_PyTorch.py
@@ -1,6 +1,8 @@
 import math
 import torch
 
+import datetime
+
 import RisiContraction_18
 
 import sys
@@ -18,16 +20,37 @@ tensor = torch.randn(N, N, N, nChannels).type(torch.DoubleTensor).to(device = 'c
 adj = torch.randn(N, N).type(torch.DoubleTensor).to(device = 'cuda')
 
 # Forward -- Pythonic
-value = RisiContraction_18_Op.apply(tensor, adj)
-print("Done")
+print("Test 1")
+start_time = datetime.datetime.now()
+
+value = RisiContraction_18_Op.apply(tensor, adj) # Call the autograd operator
+
+end_time = datetime.datetime.now()
+time_diff = end_time - start_time
+execution_time = time_diff.total_seconds() * 1000
+print("Time ellapsed:", execution_time)
 
 # Forward (CUDA extension)
+print("Test 2")
+start_time = datetime.datetime.now()
+
 value = torch.zeros(N, N, nChannels * NCONTRACTIONS).type(torch.DoubleTensor).to(device = 'cuda')
 RisiContraction_18.RisiContraction_18_forward(tensor, adj, value, NTHREADS)
-print("Done")
+
+end_time = datetime.datetime.now()
+time_diff = end_time - start_time
+execution_time = time_diff.total_seconds() * 1000
+print("Time ellapsed:", execution_time)
 
 # Backward (CUDA extension)
+print("Test 3")
+start_time = datetime.datetime.now()
+
 tensor_gradient = torch.zeros(N, N, N, nChannels).type(torch.DoubleTensor).to(device = 'cuda')
 value_gradient = torch.randn(N, N, nChannels * NCONTRACTIONS).type(torch.DoubleTensor).to(device = 'cuda')
 RisiContraction_18.RisiContraction_18_backward(tensor_gradient, adj, value_gradient, NTHREADS)
-print("Done")
+
+end_time = datetime.datetime.now()
+time_diff = end_time - start_time
+execution_time = time_diff.total_seconds() * 1000
+print("Time ellapsed:", execution_time)
